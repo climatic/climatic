882ba3fedcc187dda351180454860bf50aad8a22
Peter Hancock <peter.hancock@gmail.com>
2013-04-02 16:18:52 +0100

    Prototyping

---
+++ src/main/groovy/org/climatic/TaskApp.groovy
@@ -0,0 +1,206 @@
+package org.climatic
+
+public class TaskApp {
+
+  private final tasks = [:]
+
+  private final beforeTasks = []
+    
+  private static class TaskGraphBuilder {
+    
+    private final tasks
+      
+    private currentTask
+    
+    public TaskGraphBuilder(tasks) {
+      this.tasks = tasks
+    }
+    
+    public void configure(Closure... taskConfigurations) {
+      task(null, taskConfigurations)
+    }
+    
+    Task task(String name, Closure... taskConfigurations) {
+        Task task = resolveTaskFromName(name)
+        if (taskConfigurations) {
+          def parentTask = currentTask
+          currentTask = task
+          taskConfigurations.each { taskConfiguration ->
+            taskConfiguration.delegate = this
+            taskConfiguration()
+          }
+          currentTask = parentTask
+        }
+        task
+    }
+  
+    private resolveTaskFromName(name) {
+      def task     
+      if (currentTask) {
+        task = currentTask.createTask(name)
+      } else {
+        task = getRootTask() ?: Task.createRootTask()
+      }
+      tasks[task.qualifiedName] = task
+      task
+    }
+    
+    private getRootTask() {
+      tasks[':']
+    }
+
+    public void dependsOn(String... tasks) {
+      currentTask.dependsOn.addAll tasks
+    }
+
+    public void configureCli(cli) {
+      currentTask.cliConfig << cli
+    }
+
+    public void handleCli(cli) {
+      currentTask.cliHandle << cli
+    }
+
+    public void onExecute(job) {
+      currentTask.runners << job
+    }  
+    
+  }
+
+  private static class Task {
+    final name
+    final qualifiedName
+    final dependsOn = []
+    final cliConfig = []
+    final cliHandle = []
+    final runners = []
+
+    def subTasks = []
+
+    private Task(name, qualifiedName) {
+      this.name = name
+      this.qualifiedName = qualifiedName
+    }
+    
+    static Task createRootTask() {
+      new Task(':', ':')
+    }
+
+    Task createTask(name) {
+      def task = new Task(name, fullyQualify(name))
+      subTasks << task
+      task
+    }
+
+    private fullyQualify(taskName) {
+      qualifiedName == ':' ? ':' + taskName : qualifiedName + ':' + taskName
+    }
+
+    def getDependencies(tasks, upstream = [this].toSet()) {
+       dependsOn.unique().collect {
+         def task = tasks[it]
+         if(!task) {
+           throw new RuntimeException("Task $it does not exist")
+         }
+        task
+      }.each {
+        if (upstream.contains(it)) {
+          throw new RuntimeException("Cyclic dependencies found")
+        }
+        upstream << it
+      }.collect {
+          it.getDependencies(tasks, upstream)
+      }.flatten().unique() + [this]
+    }
+
+    def asMap() {
+        def details = [name: name, qn: qualifiedName]
+      if (dependsOn) {
+        details.dependsOn = dependsOn.join(', ')
+      }
+      if (subTasks) {
+        details.subTasks = subTasks*.asMap()
+      }
+      details
+    }
+
+  }
+
+  TaskApp configure(Closure... taskConfigurations) {
+    new TaskGraphBuilder(tasks).configure(taskConfigurations)
+    this
+  }
+
+  TaskApp onTaskBegin(Closure taskBeginAction) {
+    beforeTasks << taskBeginAction
+    this
+  }
+
+  private static class Help extends RuntimeException {}
+
+  def help() {
+    throw new Help()
+  }
+
+  def run(String... args) {
+    def taskName = ':'
+    def task = tasks[taskName]
+    def (argz, config) = configTask(task, args)
+    if (argz) {
+      taskName += argz.head()
+      def nextTask = tasks[taskName]
+      while (nextTask) {
+        task = nextTask
+          (argz, config) = configTask(task, argz.tail(), config)
+        if (argz) {
+          taskName += ':' + argz.head()
+          nextTask = tasks[taskName]
+        } else {
+          nextTask = null
+        }
+      }
+    }
+    println 'Task graph execution beginning ...'
+    def tasks = task.getDependencies(tasks)
+    runTasks(tasks, config, argz)
+    println 'Task graph execution completed.'
+    task
+  }
+
+  private configTask(task, args, config = new ConfigObject()) {
+    def cli = new CliBuilder()
+    task.cliConfig.each {
+      cli.with it
+    }
+    def options = cli.parse(args)
+    try {
+      task.cliHandle.each { cliHandler ->
+        cliHandler(options, config)
+      }
+      } catch (Help h) {
+        cli.usage()
+          throw h
+      }
+    config[task.qualifiedName].cli = cli
+    [options.arguments(), config]
+  }
+
+  private runTasks(tasks, config, argz) {
+    tasks.each { task ->
+      beforeTasks.each {
+        it(task, config, argz)
+      }
+      try {
+      task.runners.each {
+        it.delegate = this
+
+        it(task, config, argz)
+      }
+      } catch (Help h) {
+        config[task.qualifiedName].cli.usage
+          throw h
+      }
+    }
+  }
+
+}
---
+++ src/test/groovy/org/climatic/TaskAppSpec.groovy
@@ -0,0 +1,72 @@
+package org.climatic
+
+import spock.lang.Specification
+
+class TaskAppSpec extends Specification {
+  
+  def 'full test'() {
+    def app = new TaskApp()
+    app.configure {
+      onExecute { task, config, args ->
+        help()
+      }
+      configureCli {
+        setUsage '''
+          T A S K   A P P
+          '''
+        h longOpt: 'help', 'usage information'
+        t longOpt: 'test.home', args:1, argName:'dir', 'Test Suite home'
+      }
+      handleCli { options, config ->
+        if(options.h) {
+          help()
+          config.h = 'help'
+        }
+        if(!options.t) {
+          println 'no test suite set'
+            help()
+          } else {
+            config.testHome = options.t
+          }
+        config.root = 'root value'
+      }
+      task('suite') {
+        onExecute { task, config, args ->
+          println config.root
+        }
+        task('show-all')
+      }
+      task('test') {
+        dependsOn ':group1'
+
+        handleCli { options, config ->
+        }
+        onExecute { task, config, args ->
+          println '    group options'
+        }
+        task('clean') {
+          onExecute { task, config, args ->
+            println "    executing task '$task.name'"
+          }
+        }
+        task('run') {
+          dependsOn(':test:clean', ':suite:show-all')
+          onExecute { task, config, args ->
+            println "    executing task '$task.name' - config '$config', args '$args'"
+          }
+        }
+        task('run-ouput') {
+          onExecute { task, config, args ->
+            println "    executing task '$task.name' - config '$config', args '$args'"
+          }
+        }
+      }
+    }
+    /*app.onTaskBegin { task, config, args ->
+      println "  before executing $task.name $config"
+    }*/
+    app.run('-t /tmp test run'.split())
+      expect:
+    true
+  }
+}
