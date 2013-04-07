c1bedd657167279dd9c217ec2f4ebcb8c2982204
Peter Hancock <peter.hancock@gmail.com>
2013-04-07 12:51:01 +0100

    Fixed test and made it useful

--- src/test/groovy/org/climatic/TaskAppSpec.groovy
+++ src/test/groovy/org/climatic/TaskAppSpec.groovy
@@ -3,70 +3,46 @@ package org.climatic
 import spock.lang.Specification
 
 class TaskAppSpec extends Specification {
-  
-  def 'full test'() {
-    def app = new TaskApp()
-    app.configure {
-      onExecute { task, config, args ->
-        help()
-      }
-      configureCli {
-        setUsage '''
-          T A S K   A P P
-          '''
-        h longOpt: 'help', 'usage information'
-        t longOpt: 'test.home', args: 1, argName: 'dir', 'Test Suite home'
-      }
-      handleCli { options, config ->
-        if(options.h) {
-          help()
-          config.h = 'help'
-        }
-        if(!options.t) {
-          println 'no test suite set'
-            help()
-          } else {
-            config.testHome = options.t
-          }
-        config.root = 'root value'
-      }
-      task('suite') {
-        onExecute { task, config, args ->
-          println config.root
-        }
-        task('show-all')
-      }
-      task('test') {
-        dependsOn ':group1'
 
-        handleCli { options, config ->
+  private final writer = new StringWriter()
+
+  private final taskConfiguration = {
+      task('test') {
+        configureCliBuilder { cliBuilder ->
+          cliBuilder.with {
+            e longOpt: 'echo', args: 1, argName: 'message', 'message'
+          }
+        }
+        handleOptions { options, config ->
+          if(!options.e) {
+            help 'No message'
+          }
+          config.message = options.e
         }
         onExecute { task, config, args ->
-          println '    group options'
-        }
-        task('clean') {
-          onExecute { task, config, args ->
-            println "    executing task '$task.name'"
-          }
-        }
-        task('run') {
-          dependsOn(':test:clean', ':suite:show-all')
-          onExecute { task, config, args ->
-            println "    executing task '$task.name' - config '$config', args '$args'"
-          }
-        }
-        task('run-ouput') {
-          onExecute { task, config, args ->
-            println "    executing task '$task.name' - config '$config', args '$args'"
-          }
+          writer << config.message
         }
       }
+
     }
-    /*app.onTaskBegin { task, config, args ->
-      println "  before executing $task.name $config"
-    }*/
-    app.run('-t /tmp test run'.split())
-      expect:
-    true
+
+  def 'task executed'() {
+    def app = new TaskApp('app')
+    app.writer = writer
+    app.configure taskConfiguration
+    app.run(['test', '-e', 'testing testing'] as String[])
+
+    expect:
+        'testing testing' == writer.toString()
+  }
+
+  def 'task configured incorrectly'() {
+    def app = new TaskApp('app')
+    app.writer = writer
+    app.configure taskConfiguration
+    app.run(['test'] as String[])
+
+    expect:
+        writer.toString().startsWith('\nNo message')
   }
 }
